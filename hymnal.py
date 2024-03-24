e3f81a8ba06235a17e8112f91a6a1e5a045878f1
navi088 <ivanvicttoria_8@hotmail.com>
2024-03-24 12:59:26 -0700

    songs added 24-03-24

--- hymnal.py
+++ hymnal.py
@@ -89,7 +89,6 @@ hymn = { # FOR EACH LETTER OF LIRYCS YOU HAVE TO GIVE 2-4 BLANK SPACES IN CHORDS
                 all_chords['Do'],all_chords['Do7'],all_chords['Rem'],all_chords['Mim'],all_chords['Fa'],all_chords['Sol'],all_chords['Lam']
             ]
         },
-
         'lo debes compartir': {
             'name': 'Lo debes compartir',
             'tone': 'Re',
@@ -166,7 +165,6 @@ hymn = { # FOR EACH LETTER OF LIRYCS YOU HAVE TO GIVE 2-4 BLANK SPACES IN CHORDS
                 all_chords['Re'],all_chords['Mim'],all_chords['Fa#m'],all_chords['Sol'],all_chords['La'],all_chords['Sim']
             ]
         },
-
         'el espiritu de dios esta': {
             'name': 'El espiritu de Dios esta',
             'tone': 'Re',
@@ -205,6 +203,134 @@ hymn = { # FOR EACH LETTER OF LIRYCS YOU HAVE TO GIVE 2-4 BLANK SPACES IN CHORDS
             'chord_image': [
                 all_chords['Re'],all_chords['Re7'],all_chords['Mim'],all_chords['Fa#m'],all_chords['Sol'],all_chords['La'],all_chords['Sim']
             ]
+        },        
+        'majestuoso': {
+            'name': 'Majestuoso',
+            'tone': 'Re',
+            'lirycs': [
+                ' ',
+                ' ',
+                ' ',
+                'Majestuoso, poderoso',
+                'digno de loor',
+                'proclamemos su grandeza hoy',
+                ' ',#6
+                '*** Coro ***',#7
+                'Jesucristo es rey',
+                'Jesucristo es, es Rey',
+                'postrados hoy ante sus pies',
+                'Jesucristo es Rey',
+                ' ',#12
+                'Jesucristo es rey',
+                'Jesucristo es, es Rey',
+                'postrados hoy ante sus pies',
+                'Jesucristo es Rey',
+                '// Jesucristo es Rey //'
+            ],
+            'chords': [
+                'Re         La      Sol    Re',
+                'Sol      Fa#m  La',
+                ' ',
+                'Re         La      Sol    Re',
+                'Sol      Fa#m  La',
+                'Re           La      Sol         Re       La',
+                ' ',#6
+                ' ',#7
+                'Sol          La      Fa#m',
+                'Sol          La      Fa#m',
+                'Sol                          La      Fa#m      Si',
+                'Sol          La      Re/Re7',
+                ' ',#12
+                'Sol          La      Fa#m',
+                'Sol          La      Fa#m',
+                'Sol                          La      Fa#m      Si',
+                'Sol          La      Re      Sim',
+                '    Mim           La      Re'
+            ],
+            'chord_image': [
+                all_chords['Re'], all_chords['Re7'], all_chords['Mim'], all_chords['Fa#m'], 
+                all_chords['Sol'], all_chords['Si'], all_chords['Sim']
+            ]
+        },
+        'mi amado jesus': {
+            'name': 'Mi amado Jesus',
+            'tone': 'Do',
+            'lirycs': [
+                '// Mi amado Jesús',
+                'mi compañero inseparable',
+                'la confianza de mi vida',
+                'es el Dios que me fortalece',
+                'el motivo de mi alma eres tú //',
+                ' ',
+                '*** Coro ***',
+                'Te amo',
+                'mi Jesús, tú eres mi fuerza y mi',
+                'esperanza',
+                'la única esperanza es tu amor,',
+                'inevitable',
+                '¿a quien tengo yo',
+                'en el cielo sino a ti y tu grande',
+                'amor?',
+                'Haz llenado mi existecia, oh',
+                'Jesús',
+                'mi dueño eres tú'
+            ],
+            'chords': [
+                '    Do      Lam      Mim',
+                'Fa              Sol          Do',
+                'Fa                               Sol',
+                '          Mim                Lam',
+                'Fa                           Sol          Do',
+                ' ',
+                ' ',
+                '      Sol',
+                '        Fa                         Sol',
+                '    Do',
+                '      Fa            Do                  Sol',
+                '    Do',
+                '        Sol',
+                '            Fa            Sol',
+                '    Do',
+                '            Fa                    Do',
+                '  Sol',
+                '                          Do'
+            ],
+            'chord_image': [
+                all_chords['Do'], all_chords['Mim'], all_chords['Fa'], all_chords['Sol'],
+                all_chords['Lam'],
+            ]
+        },
+        'tu amor por mi': {
+            'name': 'Tu amor por mi',
+            'tone': 'La',
+            'lirycs': [
+                '// Tu amor por mi',
+                'es más dulce que la miel',
+                'y tu misericordia es',
+                'nueva cada día //',
+                ' ',
+                '*** Coro ***',
+                '// Es po eso que te alabo',
+                'es por eso que te sirvo',
+                'es por eso que te doy todo',
+                'mi amor //'
+            ],
+            'chords': [
+                '    Lam               Mim',
+                '              Lam               Mim',
+                '              Rem           Do',
+                'Lam               Sol',
+                ' ',
+                ' ',
+                '                Fa               Do',
+                '             La#              Fa',
+                '             Do               Rem',
+                '        Sol'
+            ],
+            'chord_image': [
+                all_chords['Do'], all_chords['Rem'], all_chords['Mim'], all_chords['Fa'], 
+                all_chords['Sol'],all_chords['La#'],all_chords['Lam']
+            ]
         }
-    }
+    } # THE DICTIONARY ENDS HERE
 }
